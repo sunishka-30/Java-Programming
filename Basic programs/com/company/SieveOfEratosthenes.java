030e648a2db336087a3094dae77c4b77ac63c9dd
Sunishka Sharma <sunishka2002@gmail.com>
2021-08-26 21:41:49 +0530

    sieveOfEratosthenes

---
+++ Basic programs/com/company/SieveOfEratosthenes.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SieveOfEratosthenes {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter range to check prime numbers:");
+
+        int m = sc.nextInt();
+        isPrime(m);
+
+    }
+    static void isPrime( int m)
+    {
+        boolean [] arr = new boolean[m+1];
+        Arrays.fill(arr,true);
+        arr[0]=false;
+        arr[1]=false;
+        for(int i=2;i*i<=m;i++)
+        {
+            for(int j=2*i;j<=m;j+=i)
+            {
+                arr[j]=false;
+
+            }
+        }
+        System.out.println("The prime no.s till "+m+" are:");
+        for (int i = 2; i <= m; i++)
+        {
+            if (arr[i])
+                System.out.print(i + " ");
+        }
+
+    }
+}
